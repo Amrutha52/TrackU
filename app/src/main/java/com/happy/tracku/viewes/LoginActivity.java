41a926b9a5d1a2cadd3f25cfd14ccacaec7a3c6a
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-10-09 16:08:06 +0530

    custom trust is added

---
+++ app/src/main/java/com/happy/tracku/viewes/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.happy.tracku.viewes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.happy.tracku.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
