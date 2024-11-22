c637994fb342c405c91e63538aabffc0bfc5c162
5581 <amrutha.it@maxvaluecredits.com>
2024-11-22 17:26:58 +0530

    photo punching history onprogress

--- app/src/main/java/com/happy/tracku/viewes/PhotoPunchHistoryActivity.java
+++ app/src/main/java/com/happy/tracku/viewes/PhotoPunchHistoryActivity.java
@@ -1,7 +1,16 @@
 package com.happy.tracku.viewes;
 
+import static com.happy.tracku.utils.Const.URL_MANUAL_PUNCH;
+import static com.happy.tracku.utils.Const.URL_PUNCH_HISTORY;
+import static com.happy.tracku.utils.Const.USING_IP;
+
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
@@ -12,26 +21,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
 import com.happy.tracku.R;
+import com.happy.tracku.db.DbHelper;
+import com.happy.tracku.gson.login.LoginStatusJson;
+import com.happy.tracku.gson.photopunchhistoryjson.Punchinghistoryjson;
+import com.happy.tracku.gson.photopunchingjson.Photopunchingjson;
+import com.happy.tracku.ssl.CustomTrust;
+import com.happy.tracku.utils.Const;
 import com.happy.tracku.utils.Fns;
+
+import org.json.JSONObject;
 
 import java.util.Calendar;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class PhotoPunchHistoryActivity extends AppCompatActivity
 {
-    TextView fromDateTV, toDateTV;
-    String fromDateString, toDateString;
+    TextView monthTV, yearTV;
+    //String fromDateString, toDateString;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_punch_history);
 
-        fromDateTV = findViewById(R.id.from_date);
-        toDateTV = findViewById(R.id.to_date);
+        monthTV = findViewById(R.id.monthTV);
+        yearTV = findViewById(R.id.yearTV);
 
-        fromDateTV.setOnClickListener(new View.OnClickListener() {
+        monthTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // on below line we are getting
@@ -40,9 +66,9 @@ public class PhotoPunchHistoryActivity extends AppCompatActivity
 
                 // on below line we are getting
                 // our day, month and year.
-                int year = c.get(Calendar.YEAR);
+               // int year = c.get(Calendar.YEAR);
                 int month = c.get(Calendar.MONTH);
-                int day = c.get(Calendar.DAY_OF_MONTH);
+               // int day = c.get(Calendar.DAY_OF_MONTH);
 
                 // on below line we are creating a variable for date picker dialog.
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
@@ -53,20 +79,21 @@ public class PhotoPunchHistoryActivity extends AppCompatActivity
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
                                 // on below line we are setting date to our edit text.
-                                fromDateTV.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                               // fromDateTV.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                                monthTV.setText(monthOfYear);
 
                             }
                         },
                         // on below line we are passing year,
                         // month and day for selected date in our date picker.
-                        year, month, day);
+                        0, month, 0);
                 // at last we are calling show to
                 // display our date picker dialog.
                 datePickerDialog.show();
             }
         });
 
-        toDateTV.setOnClickListener(new View.OnClickListener() {
+        yearTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // on below line we are getting
@@ -76,8 +103,8 @@ public class PhotoPunchHistoryActivity extends AppCompatActivity
                 // on below line we are getting
                 // our day, month and year.
                 int year = c.get(Calendar.YEAR);
-                int month = c.get(Calendar.MONTH);
-                int day = c.get(Calendar.DAY_OF_MONTH);
+               // int month = c.get(Calendar.MONTH);
+               // int day = c.get(Calendar.DAY_OF_MONTH);
 
                 // on below line we are creating a variable for date picker dialog.
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
@@ -88,13 +115,13 @@ public class PhotoPunchHistoryActivity extends AppCompatActivity
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
                                 // on below line we are setting date to our edit text.
-                                toDateTV.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                                yearTV.setText(year);
 
                             }
                         },
                         // on below line we are passing year,
                         // month and day for selected date in our date picker.
-                        year, month, day);
+                        year, 0, 0);
                 // at last we are calling show to
                 // display our date picker dialog.
                 datePickerDialog.show();
@@ -108,15 +135,147 @@ public class PhotoPunchHistoryActivity extends AppCompatActivity
         {
             case R.id.pull_punch_data:
             {
-                fromDateString = Fns.convertFormatDate(fromDateTV.getText().toString(), "dd-MM-yyyy", "yyyy-MM-dd");
-                Log.e("Log", "fromDateString" + fromDateString);
+                new GetPunchingHistory(this, yearTV.getText().toString(), monthTV.getText().toString()).execute();
+            }
+            break;
+        }
+    }
 
-                toDateString = Fns.convertFormatDate(toDateTV.getText().toString(), "dd-MM-yyyy", "yyyy-MM-dd");
-                Log.e("Log", "toDateString" + toDateString);
+    private static class GetPunchingHistory extends AsyncTask<String, String, String>
+    {
+        OkHttpClient okHttpClient;
+        String url;
+        Request request;
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        PhotoPunchHistoryActivity mContext;
+        ProgressDialog pd;
+        SharedPreferences shp;
+        String failureMsg, resultString;
+        Punchinghistoryjson punchinghistoryjson;
+        int status;
+        String year, month;
+
+        RecyclerView punchingHistoryRecyclerview;
+        public GetPunchingHistory(PhotoPunchHistoryActivity mContext, String year, String month)
+        {
+            this.mContext = mContext;
+            this.year = year;
+            this.month = month;
+
+            CustomTrust customTrust = new CustomTrust(mContext);
+            OkHttpClient client = customTrust.getClient();
+            okHttpClient = client;
+                    /*= new OkHttpClient.Builder()
+                    .connectTimeout(180, TimeUnit.SECONDS)
+                    .callTimeout(180, TimeUnit.SECONDS)
+                    .readTimeout(180, TimeUnit.SECONDS)
+                    .build();*/
+            pd = new ProgressDialog(mContext);
+            shp = mContext.getSharedPreferences(Const.Shared_Pref_name, MODE_PRIVATE);
+
+
+            pd.setTitle("Please wait");
+            pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            pd.setMessage("wait...");
+            pd.setCancelable(false);
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+
+            try {
+
+                pd.show();
+
+
+            } catch (Exception e) {
+
+                Log.e("Log", "Exception", e);
+
+            }
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                JSONObject photoPunchHistoryObj = new JSONObject();
+                photoPunchHistoryObj.put("createdBy", shp.getString(Const.Shp_Employee_Code, ""));
+                photoPunchHistoryObj.put("employeeCode", shp.getString(Const.Shp_Employee_Code, ""));
+                photoPunchHistoryObj.put("year", Integer.parseInt(year));
+                photoPunchHistoryObj.put("month", Integer.parseInt(month));
+                photoPunchHistoryObj.put("isCompressed", 0);
+
+                url = USING_IP + URL_PUNCH_HISTORY;
+                Log.e("Log", "photoPunchHistoryURL" + url);
+                Log.e("Log", "photoPunchHistoryJsonObject" + photoPunchHistoryObj);
+
+                RequestBody body = RequestBody.create(photoPunchHistoryObj.toString(), JSON);
+                request = new Request.Builder()
+                        //.header("X-Client-Type", "Android")
+                        .url(url)
+                        .post(body)
+                        .build();
+
+                Response response = okHttpClient.newCall(request).execute();
+                Log.e("Log", "response" + response);
+
+                if (!response.isSuccessful()) {
+
+                    return "failure";
+
+                }
+
+                resultString = response.body().string();
+                Log.e("Log", "punch history result string" + resultString);
+
+                Gson gson = new Gson();
+
+                punchinghistoryjson = gson.fromJson(resultString, Punchinghistoryjson.class);
+                Log.e("Log", "punchinghistoryjson" + punchinghistoryjson);
 
 
             }
-            break;
+            catch (Exception e)
+            {
+                Log.e("Log", "Exception", e);
+                return "failure";
+            }
+            return "success";
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+
+            super.onPostExecute(s);
+            pd.dismiss();
+
+            if (s.equals("success"))
+            {
+                punchingHistoryRecyclerview =  mContext.findViewById(R.id.punch_history_recycler_view);
+                List<EmiChartDetail> emiChartDetailList = emiChartDetailsJson.getData().getEmiChartDetails();
+                Log.e("Log", "emiChart DetailList size"+emiChartDetailList.size());
+
+                EmiChartAdapter emiChartAdapter = new EmiChartAdapter(context, emiChartDetailList);
+                Log.e("Log", "emiChart adapter" + emiChartAdapter);
+
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+
+                emiTrackRecyclerView.setLayoutManager(layoutManager);
+                emiTrackRecyclerView.setItemAnimator(new DefaultItemAnimator());
+                emiTrackRecyclerView.setAdapter(emiChartAdapter);
+
+            }
+            else
+            {
+                Toast.makeText(context, "Fetching Failed", Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 }
