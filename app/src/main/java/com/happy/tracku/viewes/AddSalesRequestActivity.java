b3121196f8c468fbeb914ca1967c1488f7c1bb2b
amrutha <amruthalalk258@gmail.com>
2025-07-19 07:44:34 +0530

    sales request changes

--- app/src/main/java/com/happy/tracku/viewes/AddSalesRequestActivity.java
+++ app/src/main/java/com/happy/tracku/viewes/AddSalesRequestActivity.java
@@ -861,9 +861,9 @@ public class AddSalesRequestActivity extends AppCompatActivity
             if (s.equals("success"))
             {
                 statusMessage = sendSalesRequestJson.getData().getSendSalesRequestStatus().get(0).getStatusMsg();
-                // Fns.neutralAlert("Alert", statusMessage, context.get());
+                 Fns.neutralAlert("Alert", statusMessage, context.get());
 
-                AlertDialog.Builder adb = new AlertDialog.Builder(context.get());
+              /*  AlertDialog.Builder adb = new AlertDialog.Builder(context.get());
 
                 TextView titletxtview = new TextView(context.get());
                 titletxtview.setText("Alert");
@@ -923,6 +923,8 @@ public class AddSalesRequestActivity extends AppCompatActivity
                 });
                 AlertDialog ad = adb.create();
                 ad.show();
+
+               */
 
 
             }
