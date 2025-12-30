044bf4a8ea29a6551ca08bf290f04fad0d550e38
5581 <developer.1@maxvaluecredits.com>
2025-12-30 13:23:42 +0530

    Release 1.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.happy.tracku"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.17"
+        versionCode = 18
+        versionName = "1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
